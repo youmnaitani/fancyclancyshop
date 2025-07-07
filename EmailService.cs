1666c1f31570fcac724fa64096bece73c58c54b5
WEB <youmnaitani2005@gmail.com>
2025-07-07 13:03:37 +0300

    Add files via upload

---
+++ EmailService.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace fancyclancyshop.Services
+{
+    public class EmailService
+    {
+        public async Task SendConfirmationEmail(string toEmail, string subject, string body)
+        {
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("your-email@gmail.com", "your-app-password"),
+                EnableSsl = true,
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("your-email@gmail.com"),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = false,
+            };
+            mailMessage.To.Add(toEmail);
+
+            await smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
